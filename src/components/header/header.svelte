e2c918eeb2bfa5a53ee58dcc7ee9774067a12e4b
Antonio <antoniojosedonishung@gmail.com>
2022-11-28 19:08:38 -0500

    Added relative path for pages

--- src/components/header/header.svelte
+++ src/components/header/header.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { base } from '$app/paths';
 	import { Navbar, NavBrand, NavLi, NavUl, NavHamburger, Skeleton } from 'flowbite-svelte';
 	// https://api.github.com/repos/shoriwe/plasma/contents/docs/embedding/embedding.md
 </script>
@@ -9,14 +10,14 @@
 		let:hidden
 		let:toggle
 	>
-		<NavBrand href="/">
-			<img src="logo.png" class="mr-3 h-6 sm:h-9" alt="plasma" />
+		<NavBrand href="{base}/">
+			<img src="{base}/logo.png" class="mr-3 h-6 sm:h-9" alt="plasma" />
 			<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Plasma</span
 			>
 		</NavBrand>
 		<NavHamburger on:click={toggle} />
 		<NavUl {hidden}>
-			<NavLi href="/documentation">Documentation</NavLi>
+			<NavLi href="{base}/documentation">Documentation</NavLi>
 		</NavUl>
 	</Navbar>
 	<div style="height:100vh;" class="overflow-scroll pb-16">
--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 import adapter from '@sveltejs/adapter-static';
 import preprocess from "svelte-preprocess";
+const dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -12,7 +13,13 @@ const config = {
 			fallback: null,
 			precompress: false,
 			strict: true
-		})
+		}),
+		paths: {
+			base: dev ? '' : '/plasma',
+		},
+		// If you are not using a .nojekyll file, change your appDir to something not starting with an underscore.
+		// For example, instead of '_app', use 'app_', 'internal', etc.
+		appDir: 'internal',
 	},
 	preprocess: [
 		preprocess({
